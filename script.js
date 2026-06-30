003f1e997a4a48a8d78b2efaa14e2503ab7291d2
billreinhardt <billreinhardt@gmail.com>
2026-06-30 08:58:34 -0500

    Completes lessons before deployment

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-console.log('hello world');
+const main = document.querySelector("#main");
+const footer = document.querySelector("#footer");
+const btnOpen = document.querySelector("#btnOpen");
+const btnClose = document.querySelector("#btnClose");
+const menuTopNav = document.querySelector("#menuTopNav");
+const breakpoint = window.matchMedia("(width < calc(600 / 16 * 1rem))")
+
+setupTopNav();
+
+btnOpen.addEventListener("click", () => mobileMenu(true));
+btnClose.addEventListener("click", () => mobileMenu(false));
+breakpoint.addEventListener("change", () => {
+  console.log("breakpoint crossed");
+  setupTopNav();
+});
+
+function mobileMenu(value) {
+  console.log(value ? "openMobileMenu" : "closeMobileMenu");
+  btnOpen.setAttribute("aria-expanded", value);
+  menuTopNav.classList.add("animating");
+  if (value == true) {
+    main.setAttribute("inert", "");
+    footer.setAttribute("inert", "");
+    menuTopNav.removeAttribute("inert");
+    bodyScrollLockUpgrade.disableBodyScroll(menuTopNav);
+    btnClose.focus();
+  }
+  else {
+    main.removeAttribute("inert");
+    footer.removeAttribute("inert");
+    menuTopNav.setAttribute("inert", "");
+    bodyScrollLockUpgrade.enableBodyScroll(menuTopNav);
+    btnOpen.focus();
+  }
+  setTimeout(() => {
+    menuTopNav.classList.remove("animating");
+  }, 300);
+}
+
+// btnOpen.addEventListener("click", openMobileMenu);
+// function openMobileMenu() {
+//   console.log("openMobileMenu");
+// }
+
+// function closeMobileMenu() {
+//   console.log("closeMobileMenu");
+// }
+
+// console.log(breakpoint);
+
+function setupTopNav() {
+  if (breakpoint.matches) {
+    console.log("is mobile")
+    menuTopNav.setAttribute("inert", "");
+  }
+  else {
+    console.log("is tablet/desktop")
+    mobileMenu(false);
+    menuTopNav.removeAttribute("inert");
+  }
+}
